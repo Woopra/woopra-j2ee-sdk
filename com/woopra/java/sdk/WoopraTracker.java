869b3f108134a6bcba0e1fbfcc8e807b7906ee8c
antoinechkaiban <antoine.chkaiban@student.ecp.fr>
2013-10-10 15:35:32 -0700

    Major Update
    - Removed front-end tracking from parent class
    - Make sure to only send the configuration that has been set
    - No http tracking if domain not set in parent class
    - Added JavaDoc

--- com/woopra/java/sdk/WoopraTracker.java
+++ com/woopra/java/sdk/WoopraTracker.java
@@ -5,7 +5,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.Random;
 
 import com.woopra.json.JSONException;
@@ -14,19 +13,20 @@ import com.woopra.sss.test.AsyncClient;
 
 /**
  * Woopra Java SDK
- * This class represents the Java Equivalent of the JavaScript Woopra Object
+ * This class enables back-end tracking in Java for Woopra.
+ * Even though the getInstance is implemented for this class, note that the design of
+ * this object is such that a new instance should be created for each new request.
  * @author Antoine Chkaiban
  * @version 2013-09-30
  */
 public class WoopraTracker {
 	
-	private boolean hasPushed;
-	private LinkedList<WoopraEvent> events;
-	private JSONObject user;
-	private boolean userUpToDate;
-	private static JSONObject defaultConfig;
+	protected JSONObject user;
+	protected boolean userUpToDate;
+	protected JSONObject customConfig;
+	protected static JSONObject defaultConfig;
 	protected JSONObject currentConfig;
-	private JSONObject customConfig;
+	private static WoopraTracker instance = null;
 	public static final String DOMAIN = "domain",
             COOKIE_NAME = "cookie_name",
             COOKIE_DOMAIN = "cookie_domain",
@@ -65,15 +65,35 @@ public class WoopraTracker {
 	    WoopraTracker.defaultConfig.put(WoopraTracker.USER_AGENT, "");
 	    WoopraTracker.defaultConfig.put(WoopraTracker.CURRENT_URL, "");
     }
+	
+	/**
+	 * If an instance of WoopraTracker already exists, this method returns it.
+	 * It creates a new instance otherwise.
+	 * @return WoopraTracker
+	 */
+	public synchronized final static WoopraTracker getInstance() {
+		if (WoopraTracker.instance == null) {
+    		WoopraTracker.instance = new WoopraTracker();
+    	}
+    	return WoopraTracker.instance;
+	}
 
+	/**
+	 * Public Constructor
+	 */
 	public WoopraTracker() {
 		this.currentConfig = WoopraTracker.defaultConfig;
-		this.userUpToDate = true;
 		this.user = new JSONObject();
-		this.hasPushed = false;
+		this.userUpToDate = true;
 	}
 	
-	public WoopraTracker config(String key, Object value) {
+	/**
+	 * Configures the WoopraTracker Object.
+	 * @param key	configuration key. All keys are accessible statically (ex: WoopraTracker.IDLE_TIMEOUT)
+	 * @param value	configuration value
+	 * @return Configured WoopraTracker
+	 */
+	public final WoopraTracker config(String key, Object value) {
 		if(WoopraTracker.defaultConfig.has(key)) {
 			if (WoopraTracker.defaultConfig.get(key).getClass() == value.getClass()) {
 				this.currentConfig.put(key, value);
@@ -91,7 +111,14 @@ public class WoopraTracker {
 		return this;
 	}
 	
-	public WoopraTracker config(Object[][] data) {
+	/**
+	 * Configures the WoopraTracker Object.
+	 * @param data	2D Array containing Arrays of size 2 where:<br>
+	 * 				key (String) - configuration key. All keys are accessible statically (ex: WoopraTracker.IDLE_TIMEOUT)<br>
+	 * 				value (String, int, boolean) - configuration value<br>
+	 * @return Configured WoopraTracker
+	 */
+	public final WoopraTracker config(Object[][] data) {
 		for(Object[] keyValue : data) {
 			String key = (String) keyValue[0];
 			Object value = keyValue[1];
@@ -100,197 +127,142 @@ public class WoopraTracker {
 		return this;
 	}
 	
-	public WoopraTracker identify(String key, String value) {
+	/**
+	 * Identifies the user to the tracker, so that when tracking (or pushing), the identified user is passed along to Woopra.
+	 * @param key	identification key
+	 * @param value	identification value
+	 * @return WoopraTracker with identified user
+	 */
+	public final WoopraTracker identify(String key, String value) {
 		this.user.put(key, value);
 		this.userUpToDate = false;
 		return this;
 	}
-	
-	public WoopraTracker identify(String[][] data) {
-		for(String[] keyValue : data) {
-			this.user.put(keyValue[0], keyValue[1]);
+
+	/**
+	 * Identifies the user to the tracker, so that when tracking (or pushing), the identified user is passed along to Woopra.
+	 * @param data	(Object[][]) 2D Array containing Arrays of size 2 where:<br>
+	 * 				key (String) - identification key<br>
+	 * 				value (String, int, boolean) - identification value<br>
+	 * @return WoopraTracker with identified user
+	 */
+	public final WoopraTracker identify(Object[][] data) {
+		for(Object[] keyValue : data) {
+			String key = (String) keyValue[0];
+			Object value = keyValue[1];
+			this.user.put(key, value);
 		}
 		this.userUpToDate = false;
 		return this;
 	}
 	
+	/**
+	 * Tracks a page view through the back-end.<br>
+	 * To be able to track, at least the domain name (WoopraTracker.DOMAIN) will have to be configured using the public config method.
+	 * Configure the following keys (optional) for the tracking to be accurate:<br>
+	 * - WoopraTracker.IP_ADDRESS		the IP address of the user currently being tracked
+	 * - WoopraTracker.CURRENT_URL		the URL of the page being currently viewed
+	 * - WoopraTracker.COOKIE_VALUE		the value of the cookie used to identify the user
+	 * - WoopraTracker.USER_AGENT		the User-Agent of the user currently being tracked
+	 * @return WoopraTracker 
+	 */
 	public WoopraTracker track() {
-		return this.track(new WoopraEvent());
+		return this.track(null);
 	}
 	
-	public WoopraTracker track(boolean backEndProcessing) {
-		if(backEndProcessing) {
-			try {
-				this.woopraHttpRequest(true, new WoopraEvent(), new String[] {"User-Agent: ".concat(this.currentConfig.getString(WoopraTracker.USER_AGENT))});
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			return this;
-		} else {
-			return this.track();
-		}
-	}
-	
+	/**
+	 * Tracks a custom event through the back-end.
+	 * @param event	(WoopraEvent) the event to track
+	 * @return WoopraTracker 
+	 */
 	public WoopraTracker track(WoopraEvent event) {
-		if(this.events == null) {
-			this.events = new LinkedList<WoopraEvent>();
-		}
-		this.events.add(event);
+		String userAgent = this.currentConfig.getString(WoopraTracker.USER_AGENT);
+		this.woopraHttpRequest(true, event, userAgent == "" ? null : new String[] {"User-Agent: ".concat(userAgent)});
 		return this;
 	}
 	
-	public WoopraTracker track(WoopraEvent event, boolean backEndProcessing) {
-		if(backEndProcessing) {
-			try {
-				this.woopraHttpRequest(true, event, new String[] {"User-Agent: ".concat(this.currentConfig.getString(WoopraTracker.USER_AGENT))});
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			return this;
-		} else {
-			return this.track(event);
-		}
-	}
-	
+	/**
+	 * Tracks a custom event through the back-end.
+	 * @param name (String) the name of the custom event
+	 * @param properties 	(Object[][]) 2D Array containing Arrays of size 2 where:<br>
+	 * 				key (String) - property name<br>
+	 * 				value (String, int, boolean) - property value<br>
+	 * @return
+	 */
 	public WoopraTracker track(String name, Object[][] properties) {
 		return this.track(new WoopraEvent(name, properties));
 	}
 	
-	public WoopraTracker track(String name, Object[][] properties, boolean backEndProcessing) {
-		if(backEndProcessing) {
-			return this.track(new WoopraEvent(name, properties), true);
-		} else {
-			return this.track(new WoopraEvent(name, properties));
-		}
-	}
-	
+	/**
+	 * Pushes the identified user to Woopra through the back-end.
+	 * Please note that the identified user is automatically pushed with any tracking event.
+	 * Therefore, this method is useful if you need to identify a user without tracking any event.
+	 */
 	public void push() {
-		if(!this.userUpToDate) {
-			this.hasPushed = true;
+		if (! this.userUpToDate) {
+			String userAgent = this.currentConfig.getString(WoopraTracker.USER_AGENT);
+			this.woopraHttpRequest(false, null, userAgent == "" ? null : new String[] {"User-Agent: ".concat(userAgent)});
 		}
 	}
 	
-	public void push(boolean backEndProcessing) throws JSONException, IOException {
-		if(backEndProcessing) {
-			this.woopraHttpRequest(false, new WoopraEvent(), new String[] {"User-Agent: ".concat(this.currentConfig.getString(WoopraTracker.USER_AGENT))});
-		} else {
-			this.push();
+	protected final void woopraHttpRequest(boolean isTracking, WoopraEvent event, String[] headers) {
+		if (this.currentConfig.get(WoopraTracker.DOMAIN).equals("")) {
+			return;
 		}
-	}
-	
-	private String printJavaScriptConfiguration() {
-		String jsConfig = "";
-		if(this.customConfig != null) {
-			jsConfig = jsConfig.concat("   woopra.config(".concat(this.customConfig.toString()).concat(");\n"));
-			this.customConfig = null;
-		}
-		return jsConfig;
-	}
-	
-	private String printJavaScriptIndentification() {
-		String jsUser = "";
-		if(!this.userUpToDate) {
-			jsUser = jsUser.concat("   woopra.identify(".concat(this.user.toString()).concat(");\n"));
-			this.userUpToDate = true;
-		}
-		return jsUser;
-	}
-	
-	private String printJavaScriptEvents() {
-		String jsEvents = "";
-		if(this.events != null) {
-			for(WoopraEvent event : this.events){
-			   if(event.name == null) {
-				   jsEvents = jsEvents.concat("   woopra.track();\n");
-			   } else {
-				   jsEvents = jsEvents.concat("   woopra.track('".concat(event.name).concat("', ").concat(event.toString()).concat(");\n"));
-			   }
+		try {
+			String baseUrl = "http://www.woopra.com/track/";
+			String configParams = "?host=".concat(URLEncoder.encode((String) this.currentConfig.get(WoopraTracker.DOMAIN), "UTF-8"));
+			if (! this.currentConfig.getString(WoopraTracker.COOKIE_VALUE).equals("")) {
+				configParams = configParams.concat("&cookie=").concat(URLEncoder.encode( (String) this.currentConfig.get(WoopraTracker.COOKIE_VALUE), "UTF-8"));
 			}
-			this.events = null;
-		}
-		return jsEvents;
-	}
-	
-	private void woopraHttpRequest(boolean isTracking, WoopraEvent event, String[] headers) throws JSONException, MalformedURLException, IOException {
-		String baseUrl = "http://www.woopra.com/track/";
-
-		//Config params
-		String configParams = "?host=".concat(URLEncoder.encode((String) this.currentConfig.get(WoopraTracker.DOMAIN), "UTF-8"));
-		configParams = configParams.concat("&cookie=").concat(URLEncoder.encode( (String) this.currentConfig.get(WoopraTracker.COOKIE_VALUE), "UTF-8"));
-		configParams = configParams.concat("&ip=").concat(URLEncoder.encode( (String) this.currentConfig.get(WoopraTracker.IP_ADDRESS), "UTF-8"));
-		configParams = configParams.concat("&timeout=").concat(URLEncoder.encode(this.currentConfig.get(WoopraTracker.IDLE_TIMEOUT).toString(), "UTF-8"));
-
-		//User params
-		String userParams = "";
-		@SuppressWarnings("unchecked")
-		Iterator<String> userKeys = this.user.keys();
-		while (userKeys.hasNext()) {
-			String key = userKeys.next();
-			String value = this.user.get(key).toString();
-			userParams = userParams.concat("&cv_").concat(URLEncoder.encode(key, "UTF-8")).concat("=").concat(URLEncoder.encode(value, "UTF-8"));
-		}
-
-		String url;
-		
-		//Just identifying
-		if ( ! isTracking ) {
-			url = baseUrl.concat("identify/").concat(configParams).concat(userParams);
-
-		//Tracking
-		} else {
-
-			//Event params
-			String eventParams = "";
-			if ( event.name != null ) {
-				eventParams = eventParams.concat("&ce_name=").concat(URLEncoder.encode(event.name, "UTF-8"));
-				@SuppressWarnings("unchecked")
-				Iterator<String> eventKeys = event.properties.keys();
-				while (eventKeys.hasNext()) {
-					String key = eventKeys.next();
-					String value = event.properties.get(key).toString();
-					eventParams = eventParams.concat("&ce_").concat(URLEncoder.encode(key, "UTF-8")).concat("=").concat(URLEncoder.encode(value, "UTF-8"));
-				}
-
+			if (! this.currentConfig.getString(WoopraTracker.IP_ADDRESS).equals("")) {
+				configParams = configParams.concat("&ip=").concat(URLEncoder.encode( (String) this.currentConfig.get(WoopraTracker.IP_ADDRESS), "UTF-8"));
+			}
+			configParams = configParams.concat("&timeout=").concat(URLEncoder.encode(this.currentConfig.get(WoopraTracker.IDLE_TIMEOUT).toString(), "UTF-8"));
+			String userParams = "";
+			@SuppressWarnings("unchecked")
+			Iterator<String> userKeys = this.user.keys();
+			while (userKeys.hasNext()) {
+				String key = userKeys.next();
+				String value = this.user.get(key).toString();
+				userParams = userParams.concat("&cv_").concat(URLEncoder.encode(key, "UTF-8")).concat("=").concat(URLEncoder.encode(value, "UTF-8"));
+			}
+			String url;
+			if ( ! isTracking ) {
+				url = baseUrl.concat("identify/").concat(configParams).concat(userParams);
 			} else {
-				eventParams = eventParams.concat("&ce_name=pv&ce_url=").concat(this.currentConfig.getString(WoopraTracker.CURRENT_URL));
+				String eventParams = "";
+				if ( event != null ) {
+					eventParams = eventParams.concat("&ce_name=").concat(URLEncoder.encode(event.name, "UTF-8"));
+					@SuppressWarnings("unchecked")
+					Iterator<String> eventKeys = event.properties.keys();
+					while (eventKeys.hasNext()) {
+						String key = eventKeys.next();
+						String value = event.properties.get(key).toString();
+						eventParams = eventParams.concat("&ce_").concat(URLEncoder.encode(key, "UTF-8")).concat("=").concat(URLEncoder.encode(value, "UTF-8"));
+					}
+				} else {
+					eventParams = eventParams.concat("&ce_name=pv");
+					if (! this.currentConfig.getString(WoopraTracker.CURRENT_URL).equals("")) {
+						eventParams = eventParams.concat("&ce_url=").concat(this.currentConfig.getString(WoopraTracker.CURRENT_URL));
+					}
+				}
+				url = baseUrl.concat("ce/").concat(configParams).concat(userParams).concat(eventParams);
 			}
-			url = baseUrl.concat("ce/").concat(configParams).concat(userParams).concat(eventParams);
+			AsyncClient.getInstance().send(new URL(url), headers);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-		AsyncClient.getInstance().send(new URL(url), headers);
 	}
 	
-	public String jsCode() {
-		String jsCode = "\n";
-		jsCode = jsCode.concat("<!-- Woopra code starts here -->\n");
-		jsCode = jsCode.concat("<script>\n");
-		jsCode = jsCode.concat("   (function(){\n   var t,i,e,n=window,o=document,a=arguments,s=\"script\",r=[\"config\",\"track\",\"identify\",\"visit\",\"push\",\"call\"],c=function(){var t,i=this;for(i._e=[],t=0;r.length>t;t++)(function(t){i[t]=function(){return i._e.push([t].concat(Array.prototype.slice.call(arguments,0))),i}})(r[t])};for(n._w=n._w||{},t=0;a.length>t;t++)n._w[a[t]]=n[a[t]]=n[a[t]]||new c;i=o.createElement(s),i.async=1,i.src=\"//static.woopra.com/js/w.js\",e=o.getElementsByTagName(s)[0],e.parentNode.insertBefore(i,e)\n   })(\"woopra\");\n");
-		jsCode = jsCode.concat(this.printJavaScriptConfiguration());
-		jsCode = jsCode.concat(this.printJavaScriptIndentification());
-		jsCode = jsCode.concat(this.printJavaScriptEvents());
-		if(this.hasPushed) {
-			jsCode = jsCode.concat("   woopra.push();\n");
-		}
-		jsCode = jsCode.concat("</script>\n");
-		jsCode = jsCode.concat("<!-- Woopra code ends here -->\n");
-		return jsCode;
-	}
-	
-	protected static String randomCookie() {
+	protected final static String randomCookie() {
 		String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		Random rnd = new Random();
 		StringBuilder sb = new StringBuilder( 12 );
@@ -301,6 +273,6 @@ public class WoopraTracker {
 	}
 	
 	protected void actualizeCookie() {
-		
+		return;
 	}
 }
--- com/woopra/java/sdk/WoopraTrackerEE.java
+++ com/woopra/java/sdk/WoopraTrackerEE.java
@@ -1,23 +1,195 @@
 package com.woopra.java.sdk;
 
+import java.util.LinkedList;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Woopra Java SDK
+ * This class enables back-end and front-end tracking in J2EE for Woopra.
+ * Even though the getInstance is implemented for this class, note that the design of
+ * this object is such that a new instance should be created for each new request.
+ * @author Antoine Chkaiban
+ * @version 2013-09-30
+ */
 public class WoopraTrackerEE extends WoopraTracker {
 	
 	private HttpServletRequest request;
+	private HttpServletResponse response;
+	protected boolean hasPushed;
+	protected LinkedList<WoopraEvent> events;
 	
-	public WoopraTrackerEE(HttpServletRequest request) {
+	/**
+	 * Public constructor
+	 * @param request (HttpServletRequest) the current request
+	 * @param response (HttpServletResponse) the current response
+	 */
+	public WoopraTrackerEE(HttpServletRequest request, HttpServletResponse response) {
 		this.request = request;
+		this.response = response;
 		this.currentConfig.put(WoopraTrackerEE.DOMAIN, this.getDomain());
 		this.currentConfig.put(WoopraTrackerEE.COOKIE_DOMAIN, this.getDomain());
 		String cookie = this.getCookie();
-		cookie = cookie != null ? cookie : WoopraTracker.randomCookie();
+		cookie = cookie != null ? cookie : WoopraTrackerEE.randomCookie();
 		this.currentConfig.put(WoopraTrackerEE.COOKIE_VALUE, cookie);
 		this.currentConfig.put(WoopraTrackerEE.IP_ADDRESS, this.getIpAddress());
 		this.currentConfig.put(WoopraTrackerEE.USER_AGENT, this.getUserAgent());
 		this.currentConfig.put(WoopraTrackerEE.CURRENT_URL, this.getCurrentUrl());
+		this.hasPushed = false;
+		this.setWoopraCookie();
+	}
+	
+	/**
+	 * Tracks a page view through the front-end.
+	 */
+	public WoopraTracker track() {
+		return this.track(new WoopraEvent());
+	}
+	
+	/**
+	 * Tracks a page view.
+	 * @param backEndProcessing (boolean) Should the page view event be tracked through the back-end?
+	 * @return WoopraTracker
+	 */
+	public WoopraTracker track(boolean backEndProcessing) {
+		if(backEndProcessing) {
+			return super.track();
+		} else {
+			return this.track();
+		}
+	}
+	
+	/**
+	 * Tracks a custom event through the front-end.
+	 * @param event	(WoopraEvent) the event to track
+	 * @return WoopraTracker 
+	 */
+	public WoopraTracker track(WoopraEvent event) {
+		if(this.events == null) {
+			this.events = new LinkedList<WoopraEvent>();
+		}
+		this.events.add(event);
+		return this;
+	}
+	
+	/**
+	 * Tracks a custom event.
+	 * @param event	(WoopraEvent) the event to track
+	 * @param backEndProcessing (boolean) Should the event be tracked through the back-end?
+	 * @return
+	 */
+	public WoopraTracker track(WoopraEvent event, boolean backEndProcessing) {
+		if(backEndProcessing) {
+			return super.track(event);
+		} else {
+			return this.track(event);
+		}
+	}
+	
+	/**
+	 * Tracks a custom event through the front-end.
+	 * @param name (String) the name of the custom event
+	 * @param properties 	(Object[][]) 2D Array containing Arrays of size 2 where:<br>
+	 * 				key (String) - property name<br>
+	 * 				value (String, int, boolean) - property value<br>
+	 * @return
+	 */
+	public WoopraTracker track(String name, Object[][] properties) {
+		return this.track(new WoopraEvent(name, properties));
+	}
+	
+	/**
+	 * Tracks a custom event.
+	 * @param name (String) the name of the custom event
+	 * @param properties 	(Object[][]) 2D Array containing Arrays of size 2 where:<br>
+	 * 				key (String) - property name<br>
+	 * 				value (String, int, boolean) - property value<br>
+	 * @param backEndProcessing (boolean) Should the event be tracked through the back-end?
+	 * @return
+	 */
+	public WoopraTracker track(String name, Object[][] properties, boolean backEndProcessing) {
+		return this.track(new WoopraEvent(name, properties), backEndProcessing);
+	}
+	
+	/**
+	 * Pushes the identified user to Woopra through the front-end.
+	 * Please note that the identified user is automatically pushed with any tracking event.
+	 * Therefore, this method is useful if you need to identify a user without tracking any event.
+	 */
+	public void push() {
+		if(!this.userUpToDate) {
+			this.hasPushed = true;
+		}
+	}
+	
+	/**
+	 * Pushes the identified user to Woopra.
+	 * Please note that the identified user is automatically pushed with any tracking event.
+	 * Therefore, this method is useful if you need to identify a user without tracking any event.
+	 * @param backEndProcessing (boolean) Should the identification be pushed through the back-end?
+	 */
+	public void push(boolean backEndProcessing) {
+		if(backEndProcessing) {
+			super.push();
+		} else {
+			this.push();
+		}
+	}
+	
+	/**
+	 * Returns the JavaScript code for configuring, identifying, tracking, and pushing.
+	 * Call this function after all front-end actions, and place its return value in your page's header.
+	 * @return (String) the JavaScript code for all front-end tracking.
+	 */
+	public String jsCode() {
+		String jsCode = "\n";
+		jsCode = jsCode.concat("<!-- Woopra code starts here -->\n");
+		jsCode = jsCode.concat("<script>\n");
+		jsCode = jsCode.concat("   (function(){\n   var t,i,e,n=window,o=document,a=arguments,s=\"script\",r=[\"config\",\"track\",\"identify\",\"visit\",\"push\",\"call\"],c=function(){var t,i=this;for(i._e=[],t=0;r.length>t;t++)(function(t){i[t]=function(){return i._e.push([t].concat(Array.prototype.slice.call(arguments,0))),i}})(r[t])};for(n._w=n._w||{},t=0;a.length>t;t++)n._w[a[t]]=n[a[t]]=n[a[t]]||new c;i=o.createElement(s),i.async=1,i.src=\"//static.woopra.com/js/w.js\",e=o.getElementsByTagName(s)[0],e.parentNode.insertBefore(i,e)\n   })(\"woopra\");\n");
+		jsCode = jsCode.concat(this.printJavaScriptConfiguration());
+		jsCode = jsCode.concat(this.printJavaScriptIndentification());
+		jsCode = jsCode.concat(this.printJavaScriptEvents());
+		if(this.hasPushed) {
+			jsCode = jsCode.concat("   woopra.push();\n");
+		}
+		jsCode = jsCode.concat("</script>\n");
+		jsCode = jsCode.concat("<!-- Woopra code ends here -->\n");
+		return jsCode;
+	}
+	
+	private String printJavaScriptConfiguration() {
+		String jsConfig = "";
+		if(this.customConfig != null) {
+			jsConfig = jsConfig.concat("   woopra.config(".concat(this.customConfig.toString()).concat(");\n"));
+			this.customConfig = null;
+		}
+		return jsConfig;
+	}
+	
+	private String printJavaScriptIndentification() {
+		String jsUser = "";
+		if(!this.userUpToDate) {
+			jsUser = jsUser.concat("   woopra.identify(".concat(this.user.toString()).concat(");\n"));
+			this.userUpToDate = true;
+		}
+		return jsUser;
+	}
+	
+	private String printJavaScriptEvents() {
+		String jsEvents = "";
+		if(this.events != null) {
+			for(WoopraEvent event : this.events){
+			   if(event.name == null) {
+				   jsEvents = jsEvents.concat("   woopra.track();\n");
+			   } else {
+				   jsEvents = jsEvents.concat("   woopra.track('".concat(event.name).concat("', ").concat(event.toString()).concat(");\n"));
+			   }
+			}
+			this.events = null;
+		}
+		return jsEvents;
 	}
 	
 	private String getIpAddress() {
@@ -57,9 +229,9 @@ public class WoopraTrackerEE extends WoopraTracker {
 		}
 	}
 	
-	public void setWoopraCookie(HttpServletResponse response) {
+	public void setWoopraCookie() {
 		Cookie cookie = new Cookie( (String) this.currentConfig.get(WoopraTrackerEE.COOKIE_NAME), (String) this.currentConfig.get(WoopraTrackerEE.COOKIE_VALUE));
 		cookie.setMaxAge(60*60*24*365*2);
-		response.addCookie(cookie);
+		this.response.addCookie(cookie);
 	}
 }
